f839bbb5c4a8faf3a11e2ff71140bef0c3783764
Dmytro Kalistratov <wobupeld@gmail.com>
2022-02-17 22:04:39 +0200

    Styles Update + initialFix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,23 +3,7 @@ import Phonebook from './Phonebook';
 export const App = () => {
   return (
     <div>
-      <Phonebook
-        initialContacts={[
-          {
-            id: 'id-1',
-            name: 'Justice the Awesome Demon',
-            number: '666-0-066',
-          },
-          { id: 'id-2', name: 'Lucifer the CEO of Hell', number: '999-1-111' },
-          {
-            id: 'id-3',
-            name: 'Pandemonica the Tired Demon',
-            number: '000-0-000',
-          },
-          { id: 'id-4', name: 'The Helltaker', number: '969-0-001' },
-        ]}
-        initialFilter={''}
-      />
+      <Phonebook />
     </div>
   );
 };
